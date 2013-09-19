5a2912e905bb54fc1ba76d8624d4db5da61436ae
Alejandro Druetta <aledruetta@gmail.com>
2013-09-19 13:57:13 -0300

    Ejercicios 2-5

---
+++ Alejandro_Druetta/ej002.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float a, b, maximo;
+
+    printf("Dame el primer número: ");
+    scanf("%f", &a);
+    printf("Dame el segundo número: ");
+    scanf("%f", &b);
+
+    if (a >= b){
+        maximo = a;
+    }
+    else {
+        maximo = b;
+    }
+
+    printf("El máximo es %f.\n", maximo);
+
+    return 0;
+}
---
+++ Alejandro_Druetta/ej005.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float a, b, x;
+
+    printf("Valor de a: ");
+    scanf("%f", &a);
+    printf("Valor de b: ");
+    scanf("%f", &b);
+
+    if (a != 0){
+        x = -b/a;
+        printf("Solución: %f\n", x);
+    }
+    else if (b != 0){
+        printf("La ecuación no tiene solución.\n");
+    }
+    else {
+        printf("La ecuación tiene infinitas soluciones.\n");
+    }
+
+    return 0;
+}
