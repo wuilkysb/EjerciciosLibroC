b2828763af840127ba645e52916b187961ef574f
Alejandro Druetta <aledruetta@gmail.com>
2013-09-21 15:58:26 -0300

    ej014.c

---
+++ Alejandro_Druetta/ej014.c
@@ -0,0 +1,20 @@
+/*
+Traduce a cadenas C las siguientes cadenas Python:
+
+1. "una cadena"
+2. ’una cadena’
+3. "una \"cadena\""
+4. ’una "cadena"’
+5. ’una \’cadena\’’
+6. "una cadena que ocupa\n dos líneas"
+7. "una cadena que \\no ocupa dos líneas"
+
+Versión C:
+
+1. "una cadena"
+2. "una cadena"
+3. "una \"cadena\""
+4. "una \"cadena\""
+5. "una \'cadena\'"
+6. "una cadena que ocupa\n dos líneas"
+7. "una cadena que \\no ocupa dos líneas"
